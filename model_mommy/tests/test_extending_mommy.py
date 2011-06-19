a6c80636e9a74a4f69f1cd18b0dbe8bddf6e9aae
Douglas Soares de Andrade <contato@douglasandrade.com>
2011-06-19 18:03:27 -0300

    Small pep8 improvements

--- model_mommy/tests/test_extending_mommy.py
+++ model_mommy/tests/test_extending_mommy.py
@@ -7,15 +7,14 @@ from model_mommy.generators import gen_from_list
 
 __all__ = ['SimpleExtendMommy', 'LessSimpleExtendMommy']
 
+
 class SimpleExtendMommy(TestCase):
 
     def test_list_generator_respects_values_from_list(self):
         age_list = range(4, 12)
 
         class KidMommy(mommy.Mommy):
-            attr_mapping = {
-                'age':gen_from_list(age_list)
-            }
+            attr_mapping = {'age': gen_from_list(age_list)}
 
         mom = KidMommy(Person)
         kid = mom.make_one()
@@ -26,9 +25,7 @@ class SimpleExtendMommy(TestCase):
         class SadPeopleMommy(mommy.Mommy):
             def __init__(self, model):
                 super(SadPeopleMommy, self).__init__(model)
-                self.type_mapping.update({
-                    BooleanField:lambda:False
-                })
+                self.type_mapping.update({BooleanField: lambda: False})
 
         assert Person._meta.get_field('happy').default is True
         sad_people_mommy = SadPeopleMommy(Person)
@@ -40,22 +37,22 @@ class SimpleExtendMommy(TestCase):
 class LessSimpleExtendMommy(TestCase):
 
     def test_unexistent_required_field(self):
-        gen_oposite = lambda x:not x
+        gen_oposite = lambda x: not x
         gen_oposite.required = ['house']
 
         class SadPeopleMommy(mommy.Mommy):
-            attr_mapping = {'happy':gen_oposite}
+            attr_mapping = {'happy': gen_oposite}
 
         mom = SadPeopleMommy(Person)
         self.assertRaises(AttributeError, mom.make_one)
 
     #TODO: put a better name
     def test_string_to_generator_required(self):
-        gen_oposite = lambda default:not default
+        gen_oposite = lambda default: not default
         gen_oposite.required = ['default']
 
         class SadPeopleMommy(mommy.Mommy):
-            attr_mapping = {'happy':gen_oposite}
+            attr_mapping = {'happy': gen_oposite}
 
         happy_field = Person._meta.get_field('happy')
         mom = SadPeopleMommy(Person)
@@ -63,10 +60,10 @@ class LessSimpleExtendMommy(TestCase):
         self.assertEqual(person.happy, not happy_field.default)
 
     def test_fail_pass_non_string_to_generator_required(self):
-        gen_age = lambda x:10
+        gen_age = lambda x: 10
 
         class MyMommy(mommy.Mommy):
-            attr_mapping = {'age':gen_age}
+            attr_mapping = {'age': gen_age}
 
         mom = MyMommy(Person)
 
