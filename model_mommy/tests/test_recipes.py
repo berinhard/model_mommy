351a54d28d7317eca1ae24d5e82b80b11ca023b3
Vanderson Mota <vanderson.mota@gmail.com>
2011-08-15 21:03:32 -0300

    4 spaces. its not ruby

--- model_mommy/tests/test_recipes.py
+++ model_mommy/tests/test_recipes.py
@@ -9,21 +9,21 @@ from datetime import date
 class TestDefiningRecipes(TestCase):
 
   def test_flat_model_recipe_definition(self):
-    recipe_attrs = {
-      'name': 'John Doe',
-      'nickname': 'joe',
-      'age': 18,
-      'bio': 'Someone in the crowd',
-      'birthday': date.today(),
-      'appointment': date.today(),
-      'blog': 'http://joe.blogspot.com',
-      'wanted_games_qtd': 4,
-    }
-    recipe = Recipe(
-      'person',
-      'model_mommy.Person',
-      **recipe_attrs
-    )
-    self.assertTrue(isinstance(recipe.mommy_instance, mommy.Mommy))
-    self.assertEqual(recipe.name, 'person')
-    self.assertEqual(recipe.mommy_instance.attr_mapping, recipe_attrs)
+      recipe_attrs = {
+        'name': 'John Doe',
+        'nickname': 'joe',
+        'age': 18,
+        'bio': 'Someone in the crowd',
+        'birthday': date.today(),
+        'appointment': date.today(),
+        'blog': 'http://joe.blogspot.com',
+        'wanted_games_qtd': 4,
+      }
+      recipe = Recipe(
+        'person',
+        'model_mommy.Person',
+        **recipe_attrs
+      )
+      self.assertTrue(isinstance(recipe.mommy_instance, mommy.Mommy))
+      self.assertEqual(recipe.name, 'person')
+      self.assertEqual(recipe.mommy_instance.attr_mapping, recipe_attrs)
