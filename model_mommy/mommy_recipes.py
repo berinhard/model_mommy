66d45ea157797873c7d97bb5e1dbcbe16acf76d5
Vanderson Mota <vanderson.mota@gmail.com>
2012-02-24 21:39:53 -0200

    four spaces. this is not ruby. =)

--- model_mommy/mommy_recipes.py
+++ model_mommy/mommy_recipes.py
@@ -7,17 +7,17 @@ from model_mommy.models import Person, Dog
 from datetime import date, datetime
 
 person = Recipe(Person,
-  name = 'John Doe',
-  nickname = 'joe',
-  age = 18,
-  bio = 'Someone in the crowd',
-  blog = 'http://joe.blogspot.com',
-  wanted_games_qtd = 4,
-  birthday = date.today(),
-  appointment = datetime.now()
+    name = 'John Doe',
+    nickname = 'joe',
+    age = 18,
+    bio = 'Someone in the crowd',
+    blog = 'http://joe.blogspot.com',
+    wanted_games_qtd = 4,
+    birthday = date.today(),
+    appointment = datetime.now()
 )
 
 dog = Recipe(Dog,
-  breed = 'Pug',
-  owner = foreign_key('person')
+    breed = 'Pug',
+    owner = foreign_key('person')
 )
