4d2864b5bc64e1f3c2f045a0c79951f1ecebdab0
Henrique Bastos <henrique@bastos.net>
2012-12-23 09:51:06 -0200

    Implement `smart_datetime` to align with different Django versions

--- model_mommy/timezone.py
+++ model_mommy/timezone.py
@@ -5,8 +5,19 @@ https://docs.djangoproject.com/en/1.4/topics/i18n/timezones/
 '''
 
 from datetime import datetime
+from django import VERSION
+from django.conf import settings
 
 try:
     from django.utils.timezone import now, utc
 except ImportError:
     now = lambda: datetime.now()
+
+
+def smart_datetime(*args):
+    value = datetime(*args)
+
+    if VERSION >= (1, 4) and settings.USE_TZ:
+        value = value.replace(tzinfo=utc)
+
+    return value
