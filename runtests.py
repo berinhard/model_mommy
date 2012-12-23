15ab89153ef8f582ce6d27f00ac2ba4fdbace03e
Henrique Bastos <henrique@bastos.net>
2012-12-23 15:56:05 -0200

    Refactor TestRunner import to `get_runner` helper
    This helper is available since Django 1.1.

--- runtests.py
+++ runtests.py
@@ -11,6 +11,16 @@ sys.path.insert(0, tests_dir)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
 
 
+def get_runner():
+    '''
+    Asks Django for the TestRunner defined in settings or the default one.
+    '''
+    from django.conf import settings
+    from django.test.utils import get_runner
+    TestRunner = get_runner(settings)
+    return TestRunner(verbosity=1, interactive=True, failfast=False)
+
+
 def runtests():
     args = sys.argv[1:]
     if args:
@@ -18,15 +28,8 @@ def runtests():
     else:
         test_labels = ['model_mommy']
 
-    try:
-        from django.test.simple import run_tests
-        result = run_tests(test_labels, 1, True)
-        sys.exit(result)
-    except ImportError:
-        from django.test.simple import DjangoTestSuiteRunner
-        test_suite = DjangoTestSuiteRunner(1, True)
-        result = test_suite.run_tests(test_labels)
-        sys.exit(result)
+    runner = get_runner()
+    sys.exit(runner.run_tests(test_labels))
 
 
 if __name__ == '__main__':
