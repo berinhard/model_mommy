b05ad584daa121c6eb084e1644f422b402081f20
Henrique Bastos <henrique@bastos.net>
2012-12-23 16:14:55 -0200

    Eliminate the need for a test_settings.py file.
    Currently the code rely on `DJANGO_SETTINGS_MODULE` to setup Django settings to run the tests.
    Implementing the function `configure_settings` we make `runtests.py` self contained.
    However, it still is possible to override the programmed settings:
    
    ```bash
    DJANGO_SETTINGS_MODULE='path.to.some_settings' ./runtests.py
    ```

--- runtests.py
+++ runtests.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python
 
-import os
+from os.path import dirname, join
 import sys
 
-parent = os.path.abspath(os.path.dirname(__file__))
-tests_dir = os.path.join(parent, 'model_mommy', 'tests')
-sys.path.insert(0, parent)
-sys.path.insert(0, tests_dir)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
+def configure_settings():
+    from django.conf import settings
+
+    # If DJANGO_SETTINGS_MODULE envvar exists the settings will be
+    # configured by it. Otherwise it will use the parameters bellow.
+    if not settings.configured:
+        params = dict(
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': ':memory:',
+                }
+            },
+            INSTALLED_APPS = (
+                'django.contrib.contenttypes',
+                'model_mommy',
+            ),
+            SITE_ID=1,
+            TEST_ROOT=join(dirname(__file__), 'model_mommy', 'tests'),
+        )
+
+        # Configure Django's settings
+        settings.configure(**params)
+
+    return settings
 
 
-def get_runner():
+def get_runner(settings):
     '''
     Asks Django for the TestRunner defined in settings or the default one.
     '''
-    from django.conf import settings
     from django.test.utils import get_runner
     TestRunner = get_runner(settings)
     return TestRunner(verbosity=1, interactive=True, failfast=False)
@@ -28,7 +47,8 @@ def runtests():
     else:
         test_labels = ['model_mommy']
 
-    runner = get_runner()
+    settings = configure_settings()
+    runner = get_runner(settings)
     sys.exit(runner.run_tests(test_labels))
 
 
