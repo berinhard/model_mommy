de8d5a07ad8ef1603de741985a29355c9d3a2bce
Henrique Bastos <henrique@bastos.net>
2013-01-03 14:20:51 -0200

    Fix package author name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     packages=["model_mommy"],
     include_package_data=True,  # declarations in MANIFEST.in
     install_requires=["Django <1.5", "PIL"],
-    author="vandersonmota",
+    author="Vanderson Mota",
     author_email="vandersonmota@gmail.com",
     url="http://github.com/vandersonmota/model_mommy",
     license="http://www.apache.org/licenses/LICENSE-2.0",
