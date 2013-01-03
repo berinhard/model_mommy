02e1a5376c162eaf64e301117411780119982c57
Henrique Bastos <henrique@bastos.net>
2013-01-03 14:27:29 -0200

    Group package meta data

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 setuptools.setup(
     name="model_mommy",
     version="0.8.1",
-    packages=["model_mommy"],
-    include_package_data=True,  # declarations in MANIFEST.in
-    install_requires=["Django <1.5", "PIL"],
     author="Vanderson Mota",
     author_email="vandersonmota@gmail.com",
     url="http://github.com/vandersonmota/model_mommy",
     license="Apache 2.0",
     description="Smart object creation facility for Django.",
+    packages=["model_mommy"],
+    include_package_data=True,  # declarations in MANIFEST.in
+    install_requires=["Django <1.5", "PIL"],
     keywords="django testing factory python",
 )
