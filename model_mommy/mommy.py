9e3e38681f776d7210fb72b6deb865c0a7cf879e
Eduardo Matos <eduardo.matos.silva@gmail.com>
2014-06-11 20:24:59 -0300

    fix build error on django 1.4

--- model_mommy/mommy.py
+++ model_mommy/mommy.py
@@ -26,6 +26,10 @@ try:
 except ImportError:
     BigIntegerField = IntegerField
 
+from django.core.exceptions import ValidationError
+from django.core.validators import (validate_ipv4_address,
+    validate_ipv6_address, validate_ipv46_address)
+
 from . import generators
 from .exceptions import ModelNotFound, AmbiguousModelName, InvalidQuantityException, RecipeIteratorEmpty
 
@@ -342,6 +346,32 @@ class Mommy(object):
                 base_kwargs[value_key] = model_instance
                 make(through_model, **base_kwargs)
 
+    def _ip_generator(self, field):
+        protocol = getattr(field, 'protocol', '').lower()
+
+        if not protocol:
+            field_validator = field.default_validators[0]
+            dummy_ipv4 = '1.1.1.1'
+            dummy_ipv6 = 'FE80::0202:B3FF:FE1E:8329'
+            try:
+                field_validator(dummy_ipv4)
+                field_validator(dummy_ipv6)
+                generator = generators.gen_ipv46
+            except ValidationError:
+                try:
+                    field_validator(dummy_ipv4)
+                    generator = generators.gen_ipv4
+                except ValidationError:
+                    generator = generators.gen_ipv6
+        elif protocol == 'ipv4':
+            generator = generators.gen_ipv4
+        elif protocol == 'ipv6':
+            generator = generators.gen_ipv6
+        else:
+            generator = generators.gen_ipv46
+
+        return generator
+
     def generate_value(self, field):
         '''
         Calls the generator associated with a field passing all required args.
@@ -365,13 +395,7 @@ class Mommy(object):
         elif field.__class__ in self.type_mapping:
             generator = self.type_mapping[field.__class__]
         elif isinstance(field, GenericIPAddressField):
-            protocol = field.protocol.lower()
-            if protocol == 'ipv4':
-                generator = generators.gen_ipv4
-            elif protocol == 'ipv6':
-                generator = generators.gen_ipv6
-            else:
-                generator = generators.gen_ipv46
+            generator = self._ip_generator(field)
         else:
             raise TypeError('%s is not supported by mommy.' % field.__class__)
 
