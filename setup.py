7035c4329e32d18ba01d180177637fd088581cd5
Vanderson Mota <vanderson.mota@gmail.com>
2015-01-17 18:32:20 -0200

    updating classifiers

--- setup.py
+++ setup.py
@@ -30,8 +30,11 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Topic :: Software Development',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 )
