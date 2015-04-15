f0fbda083e633512877bccc2246aeb55e9ca38e3
Rômulo Collopy <romulocollopy@gmail.com>
2015-04-15 12:44:23 -0300

    add default value to BooleanField to solve issue #222

--- test/generic/models.py
+++ test/generic/models.py
@@ -57,7 +57,7 @@ class Person(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CH)
     happy = models.BooleanField(default=True)
     unhappy = models.BooleanField(default=False)
-    bipolar = models.BooleanField()
+    bipolar = models.BooleanField(default=False)
     name = models.CharField(max_length=30)
     nickname = models.SlugField(max_length=36)
     age = models.IntegerField()
