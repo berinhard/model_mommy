8ee486498bf50e3e1cacdfe7e328659d812712b3
Bernardo Fontes <bernardoxhc@gmail.com>
2019-10-21 08:53:34 -0300

    Add deprecation warning

--- model_mommy/__init__.py
+++ model_mommy/__init__.py
@@ -3,5 +3,7 @@ __title__ = 'model_mommy'
 __author__ = 'Vanderson Mota'
 __license__ = 'Apache 2.0'
 
+import warnings
+warnings.warn('Important: model_mommy is no longer maintained. Please use model_bakery instead.', DeprecationWarning)
 
 from .utils import seq  # NoQA
