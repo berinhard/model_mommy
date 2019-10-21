4507e780417dd88bcf6a00c74ee17949980ce77a
Bernardo Fontes <bernardoxhc@gmail.com>
2019-10-21 18:59:11 -0300

    Link model bakery in the deprecation warning message

--- model_mommy/__init__.py
+++ model_mommy/__init__.py
@@ -4,6 +4,6 @@ __author__ = 'Vanderson Mota'
 __license__ = 'Apache 2.0'
 
 import warnings
-warnings.warn('Important: model_mommy is no longer maintained. Please use model_bakery instead.', DeprecationWarning)
+warnings.warn('Important: model_mommy is no longer maintained. Please use model_bakery instead: https://pypi.org/project/model-bakery/', DeprecationWarning)
 
 from .utils import seq  # NoQA
